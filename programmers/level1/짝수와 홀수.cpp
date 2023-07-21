630203db099e09aee80282626c5d9b9210fec54b
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2023-07-21 23:48:58 +0900

    23.07.21

---
+++ programmers/level1/짝수와 홀수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if(num%2==1||num%2==-1)
+        answer="Odd";
+    else if(num%2==0)
+        answer="Even";
+    return answer;
+}
